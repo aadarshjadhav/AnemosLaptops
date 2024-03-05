8282f6f961dce3e972b366d60ec44b9cc8ef2974
Aadarsh <aadarshjadhav@gmail.com>
2024-03-05 09:50:31 +0530

    updated after building app

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AZ6LRSMN.mjs";import"./chunk-H7PGJZRY.mjs";import"./chunk-X7ILOOES.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RYVLSYN2.mjs";import"./chunk-H7PGJZRY.mjs";import"./chunk-X7ILOOES.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
